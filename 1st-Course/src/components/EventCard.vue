f467fa4325b76c4b264d035aafd4c58692854aa8
Rian Harper <rian.harper@sapere.co.uk>
2026-01-22 15:08:27 +0000

    End of Lesson 6

--- 1st-Course/src/components/EventCard.vue
+++ 1st-Course/src/components/EventCard.vue
@@ -10,10 +10,12 @@ defineProps({
 </script>
 
 <template>
+    <RouterLink class="event-link" :to="{name: 'event-details', params: { id: event.id }}">
     <div class="event-card">
         <h2>{{ event.title }}</h2>
         <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
+    </RouterLink>
 </template>
 
 <style scoped>
@@ -27,5 +29,9 @@ defineProps({
 .event-card:hover {
     transform: scale(1.01);
     box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+    text-decoration: none;
+    color: #2c3e50;
 }
 </style>
--- 1st-Course/src/router/index.js
+++ 1st-Course/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import EventDetailsView from '../views/EventDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'event-list',
       component: EventListView
+    },
+    {
+      path: '/events/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView
     },
     {
       path: '/about',
