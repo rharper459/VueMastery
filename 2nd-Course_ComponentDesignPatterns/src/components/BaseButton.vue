2eaadd314b1b93a8cd15cc1cea23315021766f90
Rian Harper <rian.harper@sapere.co.uk>
2026-01-23 10:04:56 +0000

    End of Lesson 2_5

---
+++ 2nd-Course_ComponentDesignPatterns/src/components/BaseButton.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+})
+
+</script>
+
+<template>
+    <div>
+        <button class="button">
+            <slot name="icon-left"></slot>
+            {{ label }}
+            <slot name="icon-right"></slot>
+        </button>
+    </div>
+</template>
+
+<style scoped>
+    .button {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        padding: 12px 30px;
+        font-size: 16px;
+        font-weight: bold;
+        color: white;
+        background-color: #1b58a9;
+        border: 2px solid #fff;
+        border-radius: 10px;
+        cursor: pointer;
+        
+    }   
+</style>
--- 2nd-Course_ComponentDesignPatterns/src/views/HomeView.vue
+++ 2nd-Course_ComponentDesignPatterns/src/views/HomeView.vue
@@ -1,9 +1,40 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import BaseButton from '@/components/BaseButton.vue';
+import IconCommunity from '@/components/icons/IconCommunity.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="butttons">
+      <BaseButton label="left-Icon" >
+        <template #icon-left>
+          <IconCommunity />
+        </template>
+      </BaseButton>
+      <BaseButton label="Double-Icon" >
+        <template #icon-left>
+          <IconCommunity />
+        </template>
+        <template #icon-right>
+          <IconCommunity />
+        </template>
+      </BaseButton>
+      <BaseButton label="right-Icon" >
+        <template #icon-right>
+          <IconCommunity />
+        </template>
+      </BaseButton>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.butttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  gap: 20px;
+  
+}
+</style>
